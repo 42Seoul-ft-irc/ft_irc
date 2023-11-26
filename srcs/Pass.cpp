3270539f47216fd6cc650aa65ccbc00b4db3f16e
임원정 <wonlim@student.42seoul.kr>
2023-11-26 21:26:28 +0900

    refactor : Pass 분리 #16

---
+++ srcs/Pass.cpp
@@ -0,0 +1,35 @@
+#include "Pass.hpp"
+
+Pass::Pass(Message *msg, UserInfo &user, std::string password) : Command(msg), user(user), password(password){}
+
+Pass::~Pass() {}
+
+void Pass::execute()
+{
+	std::cout << "pass 명령어 실행\n";
+
+	if (getParameters().size() < 2)
+	{
+		// send(ERR_NEEDMOREPARAMS)
+		return;
+	}
+	else if (getParameters().size() != 2)
+	{
+		// send(PASS uses 'PASS passparameter')
+		return;
+	}
+	if (user.getPass())
+	{
+		// send(ERR_ALREADYREGISTRED)
+		return;
+	}
+	if (*getParametersBegin()++ == password)
+	{
+		user.checkPass();
+		std::cout << "password completed\n";
+	}
+	else
+	{
+		// send(ERR_PASSWDMISMATCH)
+	}
+}
