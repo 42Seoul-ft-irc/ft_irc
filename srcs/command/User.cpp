be52c5fb720f4e10f77f710fdeb0fae7f94db27a
임원정 <wonlim@student.42seoul.kr>
2023-11-28 21:36:56 +0900

    feat : USER 예외처리 #18

--- srcs/command/User.cpp
+++ srcs/command/User.cpp
@@ -4,5 +4,17 @@ User::User(Message *msg, UserInfo &user) : Command(msg), user(user) {}
 
 User::~User() {}
 
-void User::execute() {
+void User::execute()
+{
+	if (!user.getPass())
+		return;
+
+	if (user.getActive())
+		ft_send(user.getFd(), "462 :You may not reregister"); // ERR_ALREADYREGISTRED
+
+	if (getParameters().size() < 3)
+		ft_send(user.getFd(), "461 USER :Not enough parameters"); // ERR_NEEDMOREPARAMS
+
+	if (getTrailing().empty())
+		ft_send(user.getFd(), "461 USER :Not enough parameters"); // ERR_NEEDMOREPARAMS
 }
