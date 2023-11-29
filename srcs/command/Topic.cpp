d8db7f00b543338f971ed154ce19075ae956c10e
임원정 <wonlim@student.42seoul.kr>
2023-11-30 03:35:45 +0900

    feat : Topic 예외처리 #20

---
+++ srcs/command/Topic.cpp
@@ -0,0 +1,30 @@
+#include "Topic.hpp"
+
+Topic::Topic(Message *msg, UserInfo &user,
+			 std::map<std::string, Channel> channelList) : Command(msg), user(user), channelList(channelList)
+{
+}
+
+Topic::~Topic() {}
+
+void Topic::execute()
+{
+	if (!user.getActive())
+		return;
+	if (!getParameters().size()) // 461 ERR_NEEDMOREPARAMS
+	{
+		ft_send(user.getFd(), const_cast<char *>("461 TOPIC :Not enough parameters"));
+		return;
+	}
+
+	std::map<std::string, Channel>::iterator it = channelList.find(getParameters()[0]);
+	if (it == channelList.end()) // 403 ERR_NOSUCHCHANNEL
+	{
+		std::string str = "461 ";
+		str.append(getParameters()[0]);
+		str.append(" :No such channel");
+
+		ft_send(user.getFd(), const_cast<char *>(str.c_str()));
+		return;
+	}
+}
---
+++ srcs/command/Topic.hpp
@@ -0,0 +1,21 @@
+#ifndef TOPIC_HPP
+#define TOPIC_HPP
+
+#include "Command.hpp"
+#include "../UserInfo.hpp"
+#include "../utils.hpp"
+#include "../Channel.hpp"
+
+class Topic : public Command
+{
+private:
+	UserInfo &user;
+	std::map<std::string, Channel> channelList;
+
+public:
+	Topic(Message *msg, UserInfo &user, std::map<std::string, Channel> channelList);
+	~Topic();
+	void execute();
+};
+
+#endif
