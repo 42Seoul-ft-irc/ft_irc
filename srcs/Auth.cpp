21e2c0e962f4b7e775875fb8241ebab2e7d98351
임원정 <wonlim@student.42seoul.kr>
2023-12-01 21:56:48 +0900

    fix : Auth 응답 수정

--- srcs/Auth.cpp
+++ srcs/Auth.cpp
@@ -18,6 +18,7 @@ bool Auth::isAllPass()
 
 void Auth::sendReplies()
 {
+	std::string name = user.getNickname();
 	std::string reply =
 		// ":irc.local 251 nickwonlim2222 :There are 0 users and 1 invisible on 1 servers\n"
 		// ":irc.local 253 nickwonlim2222 1 :unknown connections"
@@ -25,42 +26,43 @@ void Auth::sendReplies()
 		// ":irc.local 255 nickwonlim2222 :I have 1 clients and 0 servers"
 		// ":irc.local 265 nickwonlim2222 :Current local users: 1  Max: 1"
 		// ":irc.local 266 nickwonlim2222 :Current global users: 1  Max: 1"
-		"375 :- <server> Message of the day - \n"
-		"372 :                                            /\n"
-		"372 :                         _,.------....___,.' ',.-.\n"
-		"372 :                      ,-'          _,.--\"        |\n"
-		"372 :                    ,'         _.-'              .\n"
-		"372 :                   /   ,     ,'                   `\n"
-		"372 :                  .   /     /                     ``.\n"
-		"372 :                  |  |     .                       \\.\\\n"
-		"372 :        ____      |___._.  |       __               \\ `.\n"
-		"372 :      .'    `---\"\"       ``\"-.--\"'`  \\               .  \\\n"
-		"372 :     .  ,            __               `              |   .\n"
-		"372 :     `,'         ,-\"'  .               \\             |    L\n"
-		"372 :    ,'          '    _.'                -._          /    |\n"
-		"372 :   ,`-.    ,\".   `--'                      >.      ,'     |\n"
-		"372 :  . .'\\'   `-'       __    ,  ,-.         /  `.__.-      ,'\n"
-		"372 :  ||:, .           ,'  ;  /  / \\ `        `.    .      .'/\n"
-		"372 :  j|:D  \\          `--'  ' ,'_  . .         `.__, \\   , /\n"
-		"372 : / L:_  |                 .  \"' :_;                `.'.'\n"
-		"372 : .    \"\"'                  \"\"\"\"\"'                    V\n"
-		"372 :  `.                                 .    `.   _,..  `\n"
-		"372 :    `,_   .    .                _,-'/    .. `,'   __  `\n"
-		"372 :     ) \\`._        ___....----\"'  ,'   .'  \\ |   '  \\  .\n"
-		"372 :    /   `. \"`-.--\"'         _,' ,'     `---' |    `./  |\n"
-		"372 :   .   _  `\"\"'--.._____..--\"   ,             '         |\n"
-		"372 :   | .\" `. `-.                /-.           /          ,\n"
-		"372 :   | `._.'    `,_            ;  /         ,'          .\n"
-		"372 :  .'          /| `-.        . ,'         ,           ,\n"
-		"372 :  '-.__ __ _,','    '`-..___;-...__   ,.'\\ ____.___.'\n"
-		"372 :  `\"^--'..'   '-`-^-'\"--    `-^-'`.''\"\"\"\"\"`.,^.`.--' mh\n\n"
 
-		"376 :End of message of the day.";
+		":" + serverName + " 375 " + name + " :- <server> Message of the day - \n"
+		":" + serverName + " 372 " + name + " :                                            /\n"
+		":" + serverName + " 372 " + name + " :                         _,.------....___,.' ',.-.\n"
+		":" + serverName + " 372 " + name + " :                      ,-'          _,.--\"        |\n"
+		":" + serverName + " 372 " + name + " :                    ,'         _.-'              .\n"
+		":" + serverName + " 372 " + name + " :                   /   ,     ,'                   `\n"
+		":" + serverName + " 372 " + name + " :                  .   /     /                     ``.\n"
+		":" + serverName + " 372 " + name + " :                  |  |     .                       \\.\\\n"
+		":" + serverName + " 372 " + name + " :        ____      |___._.  |       __               \\ `.\n"
+		":" + serverName + " 372 " + name + " :      .'    `---\"\"       ``\"-.--\"'`  \\               .  \\\n"
+		":" + serverName + " 372 " + name + " :     .  ,            __               `              |   .\n"
+		":" + serverName + " 372 " + name + " :     `,'         ,-\"'  .               \\             |    L\n"
+		":" + serverName + " 372 " + name + " :    ,'          '    _.'                -._          /    |\n"
+		":" + serverName + " 372 " + name + " :   ,`-.    ,\".   `--'                      >.      ,'     |\n"
+		":" + serverName + " 372 " + name + " :  . .'\\'   `-'       __    ,  ,-.         /  `.__.-      ,'\n"
+		":" + serverName + " 372 " + name + " :  ||:, .           ,'  ;  /  / \\ `        `.    .      .'/\n"
+		":" + serverName + " 372 " + name + " :  j|:D  \\          `--'  ' ,'_  . .         `.__, \\   , /\n"
+		":" + serverName + " 372 " + name + " : / L:_  |                 .  \"' :_;                `.'.'\n"
+		":" + serverName + " 372 " + name + " : .    \"\"'                  \"\"\"\"\"'                    V\n"
+		":" + serverName + " 372 " + name + " :  `.                                 .    `.   _,..  `\n"
+		":" + serverName + " 372 " + name + " :    `,_   .    .                _,-'/    .. `,'   __  `\n"
+		":" + serverName + " 372 " + name + " :     ) \\`._        ___....----\"'  ,'   .'  \\ |   '  \\  .\n"
+		":" + serverName + " 372 " + name + " :    /   `. \"`-.--\"'         _,' ,'     `---' |    `./  |\n"
+		":" + serverName + " 372 " + name + " :   .   _  `\"\"'--.._____..--\"   ,             '         |\n"
+		":" + serverName + " 372 " + name + " :   | .\" `. `-.                /-.           /          ,\n"
+		":" + serverName + " 372 " + name + " :   | `._.'    `,_            ;  /         ,'          .\n"
+		":" + serverName + " 372 " + name + " :  .'          /| `-.        . ,'         ,           ,\n"
+		":" + serverName + " 372 " + name + " :  '-.__ __ _,','    '`-..___;-...__   ,.'\\ ____.___.'\n"
+		":" + serverName + " 372 " + name + " :  `\"^--'..'   '-`-^-'\"--    `-^-'`.''\"\"\"\"\"`.,^.`.--' mh\n\n"
+
+		":" + serverName + " 376 " + name + " :End of message of the day.";
 
 	ft_send(user.getFd(), reply);
 }
 
-Auth::Auth(UserInfo &user) : user(user)
+Auth::Auth(UserInfo &user, std::string serverName) : user(user), serverName(serverName)
 {
 	if (!isNamesEmpty() && isAllPass())
 	{
