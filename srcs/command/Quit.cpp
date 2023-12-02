c0c8da4745659a93f7115ddb86c952bf80a0755a
ppparkta <pppppark2@gmail.com>
2023-12-02 17:41:31 +0900

    feat: Quit class 추가 #57

--- srcs/command/Quit.cpp
+++ srcs/command/Quit.cpp
@@ -1,3 +1,6 @@
 #include "Quit.hpp"
 
-Quit::Quit(Message *msg, UserInfo &user, std::map<std::string, Channel> *channels, std::map<int, UserInfo> *users) :
+Quit::Quit(Message *msg, UserInfo &user, std::map<std::string, Channel> *channels, std::map<int, UserInfo> *users) : Command(msg), user(user) {
+	this->channels = channels;
+	this->users = users;
+}
