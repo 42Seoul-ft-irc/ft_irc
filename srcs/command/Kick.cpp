e7187fa6937f59f58870023ca338bbb4e18b1998
jisunggi <wltjd5316@naver.com>
2023-12-04 06:13:27 +0900

    fix : 에러 응답 코드 수정

--- srcs/command/Kick.cpp
+++ srcs/command/Kick.cpp
@@ -125,13 +125,13 @@ void Kick::kickUsers(std::string parameter)
 	{
 		if (checkUsers(kickUsersName[i]))
 		{
-			//ft_send();
+			std::string msg = ":irc.local 441 " + user.getNickname() + " " + kickUsersName[i] + " " + kickChannel->getName() + " :They are not on that channel";
+			ft_send(user.getFd(), msg);
+			std::cout << msg << std::endl;
 			return ;
 		}
 		else
-		{
 			eraseUser();
-		}
 	}
 	std::string chanMsg = ":" + user.getNickname() + "!" + user.getUsername() + "@" + user.getServername() + " KICK " + kickChannel->getName() + " " + kickUser->getNickname() + " :";
 	if (!getTrailing().empty())
@@ -146,17 +146,20 @@ void Kick::execute()
 {
     if (getParameters().size() < 2)
     {
-        //ft_send();
-        return ;
+		std::string msg = "461 KICK :Not enough parameters";
+		ft_send(user.getFd(), msg);
+		return;
     }
     if (checkChannel(getParameters().at(0)))
     {
-        //ft_send(ERR_NOSUCHCHANNEL);
+        std::string msg = ":irc.local 403 " + user.getNickname() + " " + getParameters().at(0) + " :No such channel";
+		ft_send(user.getFd(), msg);
         return ;
     }
     if (checkOperator(kickChannel->getName()))
     {
-        //ft_send(ERR_CHANOPRIVSNEEDED);
+        std::string msg = ":irc.local 482 " + user.getNickname() + " " + kickChannel->getName() + " :You must be a channel operator";
+		ft_send(user.getFd(), msg);
         return ;
     }
     kickUsers(getParameters().at(1));
