95504a9d799abdd25a63ecdb0243366666ae500f
임원정 <wonlim@student.42seoul.kr>
2023-12-04 22:20:57 +0900

    fix : main 리팩토링

--- srcs/utils.hpp
+++ srcs/utils.hpp
@@ -3,8 +3,12 @@
 
 #include <vector>
 #include <string>
+#include "Server.hpp"
+
+class Server;
 
 std::vector<std::string> splitByCRLF(std::string &);
 void ft_send(int, std::string);
+void ft_quit(Server &, size_t);
 
 #endif
